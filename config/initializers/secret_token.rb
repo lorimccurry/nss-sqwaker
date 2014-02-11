798a11006c5e7a5b2bf6c9307a53aec5cc637fcc
Lori McCurry <lori.mccurry@gmail.com>
2014-02-10 19:48:52 -0600

    Initialize rails: Rails is not currently installed on this system. To get the latest version, simply type:
        $ sudo gem install rails
    
    You can then rerun your "rails" command.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Squawker::Application.config.secret_key_base = 'a19e29e6865f469238271c556ca0b8008b3388bfe9fbeeeddcb0c453caf1e083859d7c37c4c549eb88bb640efef39caaee93d98ec06e96ffd03620be55417acf'
